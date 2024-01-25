f6068b514c519643374fb985388fe448fe39dfc1
HappyNext <happyoleg69@gmail.com>
2024-01-25 11:08:59 +0200

    console: add qcommandlineparser

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 
 #include "mainwindow.h"
-
+#include <QCommandLineParser>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
+    QApplication::setApplicationName("MatViz3D");
+    QApplication::setApplicationVersion("2.01");
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.process(a);
     MainWindow w;
     w.show();
     return a.exec();
