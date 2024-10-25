49453ad51a2c80b02ed39fa35c7c349a21fafa7e
HappyNext <happyoleg69@gmail.com>
2024-10-25 12:32:25 +0300

    Algorithm: do some changes in probability algorithm

--- probability_algorithm.cpp
+++ probability_algorithm.cpp
@@ -28,11 +28,14 @@ void Probability_Algorithm::setHalfAxis()
     halfaxis_a = ui->axisALineEdit->text().toFloat();
     halfaxis_b = ui->axisBLineEdit->text().toFloat();
     halfaxis_c = ui->axisCLineEdit->text().toFloat();
-    orintation_angle = ui->orintationAngleLineEdit->text().toFloat();
+    orientation_angle_a = ui->orintationAngleLineEdit->text().toFloat();
+    orientation_angle_b = ui->lineEdit->text().toFloat();
+    orientation_angle_c = ui->lineEdit_2->text().toFloat();
 }
 
 bool Probability_Algorithm::isPointIn(double x, double y, double z)
 {
+    rotatePoint(x,y,z);
     return (pow((x - 1.5) / halfaxis_a, 2) + pow((y-1.5)  / halfaxis_b, 2) + pow((z-1.5) / halfaxis_c, 2)) <= 1.0;
 }
 
@@ -46,10 +49,55 @@ void Probability_Algorithm::setNumColors(int points)
     numColors = points;
 }
 
+double Probability_Algorithm::toRadians(double degrees)
+{
+    return degrees * (M_PI/180) ;
+}
+
+void Probability_Algorithm::rotatePoint(double& x, double& y, double& z)
+{
+    double Rx[3][3] = {
+        {1, 0, 0},
+        {0, cos(toRadians(orientation_angle_a)), -sin(toRadians(orientation_angle_a))},
+        {0, sin(toRadians(orientation_angle_a)), cos(toRadians(orientation_angle_a))}
+    };
+
+    double Ry[3][3] = {
+        {cos(toRadians(orientation_angle_b)), 0, sin(toRadians(orientation_angle_b))},
+        {0, 1, 0},
+        {-sin(toRadians(orientation_angle_b)), 0, cos(toRadians(orientation_angle_b))}
+    };
+
+    double Rz[3][3] = {
+        {cos(toRadians(orientation_angle_c)), -sin(toRadians(orientation_angle_c)), 0},
+        {sin(toRadians(orientation_angle_c)), cos(toRadians(orientation_angle_c)), 0},
+        {0, 0, 1}
+    };
+    // Промежуточные координаты после поворота вокруг оси X
+    double x1 = Rx[0][0] * x + Rx[0][1] * y + Rx[0][2] * z;
+    double y1 = Rx[1][0] * x + Rx[1][1] * y + Rx[1][2] * z;
+    double z1 = Rx[2][0] * x + Rx[2][1] * y + Rx[2][2] * z;
+
+    // Промежуточные координаты после поворота вокруг оси Y
+    double x2 = Ry[0][0] * x1 + Ry[0][1] * y1 + Ry[0][2] * z1;
+    double y2 = Ry[1][0] * x1 + Ry[1][1] * y1 + Ry[1][2] * z1;
+    double z2 = Ry[2][0] * x1 + Ry[2][1] * y1 + Ry[2][2] * z1;
+
+    // Конечные координаты после поворота вокруг оси Z
+    double x3 = Rz[0][0] * x2 + Rz[0][1] * y2 + Rz[0][2] * z2;
+    double y3 = Rz[1][0] * x2 + Rz[1][1] * y2 + Rz[1][2] * z2;
+    double z3 = Rz[2][0] * x2 + Rz[2][1] * y2 + Rz[2][2] * z2;
+
+    // Обновляем исходные координаты на новые
+    x = x3;
+    y = y3;
+    z = z3;
+}
+
 
 void Probability_Algorithm::processValues(double probability[3][3][3])
 {
-    const uint64_t N = 1000;
+    const uint64_t N = 100000000;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis(0, 3);
@@ -57,38 +105,38 @@ void Probability_Algorithm::processValues(double probability[3][3][3])
     uint64_t fileld_in[3][3][3] = {{{0}}};
     uint64_t fileld_total[3][3][3] = {{{0}}};
 
-#pragma omp parallel
-    {
-        int nthreads = omp_get_num_threads();
-        uint64_t fileld_in_local[3][3][3] = {{{0}}};
-        uint64_t fileld_total_local[3][3][3] = {{{0}}};
-
-        for (uint64_t i = 0; i < N / nthreads; i++)
+    #pragma omp parallel
         {
-            double x = dis(gen), y = dis(gen), z = dis(gen);
-            int k = std::min(std::max((int)floor(x), 0), 2);
-            int l = std::min(std::max((int)floor(y), 0), 2);
-            int m = std::min(std::max((int)floor(z), 0), 2);
+            int nthreads = omp_get_num_threads();
+            uint64_t fileld_in_local[3][3][3] = {{{0}}};
+            uint64_t fileld_total_local[3][3][3] = {{{0}}};
 
-            fileld_total_local[k][l][m]++;
-
-            if (isPointIn(x, y, z))
+            for (uint64_t i = 0; i < N / nthreads; i++)
             {
-                fileld_in_local[k][l][m]++;
-            }
-        }
+                double x = dis(gen), y = dis(gen), z = dis(gen);
+                int k = std::min(std::max((int)floor(x), 0), 2);
+                int l = std::min(std::max((int)floor(y), 0), 2);
+                int m = std::min(std::max((int)floor(z), 0), 2);
 
-#pragma omp critical
-        {
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    for (int k = 0; k < 3; k++)
-                    {
-                        fileld_in[i][j][k] += fileld_in_local[i][j][k];
-                        fileld_total[i][j][k] += fileld_total_local[i][j][k];
-                    }
+                fileld_total_local[k][l][m]++;
+
+                if (isPointIn(x, y, z))
+                {
+                    fileld_in_local[k][l][m]++;
+                }
+            }
+
+        #pragma omp critical
+                {
+                    for (int i = 0; i < 3; i++)
+                        for (int j = 0; j < 3; j++)
+                            for (int k = 0; k < 3; k++)
+                            {
+                                fileld_in[i][j][k] += fileld_in_local[i][j][k];
+                                fileld_total[i][j][k] += fileld_total_local[i][j][k];
+                            }
+                }
         }
-    }
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             for (int k = 0; k < 3; k++)
@@ -116,10 +164,86 @@ std::vector<Parent_Algorithm::Coordinate> Probability_Algorithm::Add_New_Points(
     return addedPoints;
 }
 
+void Probability_Algorithm::processValuesGrid(double probability[3][3][3])
+{
+    const uint64_t N = 1000000;
+    uint64_t n = std::round(std::cbrt(N));
+
+    double step = 3.0 / (n - 1);
+
+    uint64_t fileld_in[3][3][3] = {{{0}}};
+    uint64_t fileld_total[3][3][3] = {{{0}}};
+    uint64_t fileld_in_local[3][3][3] = {{{0}}};
+    uint64_t fileld_total_local[3][3][3] = {{{0}}};
+
+    for (uint64_t i = 0; i < n; ++i)
+    {
+        for (uint64_t j = 0; j < n; ++j)
+        {
+            for (uint64_t k = 0; k < n; ++k)
+            {
+                double x = i * step;
+                double y = j * step;
+                double z = k * step;
+
+                int k_voxel = (int)floor(x);
+                int l_voxel = (int)floor(y);
+                int m_voxel = (int)floor(z);
+
+                if( k_voxel >= 0 && k_voxel <= 2)
+                {
+                    if( l_voxel >= 0 && l_voxel <= 2)
+                    {
+                        if( m_voxel >= 0 && m_voxel <= 2)
+                        {
+                            fileld_total_local[k_voxel][l_voxel][m_voxel]++;
+                        }
+                    }
+                }
+                if (isPointIn(x, y, z))
+                {
+                    fileld_in_local[k_voxel][l_voxel][m_voxel]++;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            for (int k = 0; k < 3; k++)
+            {
+                fileld_in[i][j][k] += fileld_in_local[i][j][k];
+                fileld_total[i][j][k] += fileld_total_local[i][j][k];
+            }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                if (fileld_total[i][j][k] > 0)
+                {
+                    probability[i][j][k] = (double)fileld_in[i][j][k] / fileld_total[i][j][k];
+                }
+                else
+                {
+                    probability[i][j][k] = 0.0;
+                }
+
+                qDebug() << i << "\t" << j << "\t" << k << "\t"
+                         << fileld_in[i][j][k] << "\t" << fileld_total[i][j][k] << "\t"
+                         << probability[i][j][k] << "\n";
+            }
+        }
+    }
+    // Call the function to save the probabilities to a CSV file
+    writeProbabilitiesToCSV(probability, "D:/Project(MatViz3D)/fall2024/", N);
+}
+
 void Probability_Algorithm::Generate_Filling(int isAnimation, int isWaveGeneration)
 {
     double probability[3][3][3];
-    processValues(probability);
+    processValuesGrid(probability);
     srand(time(NULL));
     unsigned int counter_max = pow(numCubes,3);
     while (!grains.empty())
--- probability_algorithm.h
+++ probability_algorithm.h
@@ -17,19 +17,24 @@ public:
     ~Probability_Algorithm();
     void setHalfAxis();
     void Generate_Filling(int isAnimation, int isWaveGeneration);
-    bool isPointIn(double x,double y,double z);
-    void processValues(double probability[3][3][3]);
     void writeProbabilitiesToCSV(double probability[3][3][3], const QString& filePath, uint64_t N); // Method
     void setNumCubes(short int numCubes);
     void setNumColors(int numColors);
 private:
     std::vector<Coordinate> Add_New_Points(const std::vector<Coordinate>& newGrains, int pointsForThisStep);
     Ui::Probability_Algorithm *ui;
+    void processValues(double probability[3][3][3]);
+    void processValuesGrid(double probability[3][3][3]);
+    bool isPointIn(double x,double y,double z);
+    void rotatePoint(double& x, double& y, double& z);
+    double toRadians(double degress);
     int pointsinvoxel;
-    float halfaxis_a = 1;
-    float halfaxis_b = 1;
-    float halfaxis_c = 1;
-    float orintation_angle;
+    float halfaxis_a;
+    float halfaxis_b;
+    float halfaxis_c;
+    float orientation_angle_a;
+    float orientation_angle_b;
+    float orientation_angle_c;
 };
 
 #endif // PROBABILITY_ALGORITHM_H
