4bcda929d2972178910319678493e024e80dc37d
Oleksii Vodka <oleksii.vodka@gmail.com>
2025-03-07 01:23:02 +0200

    fix opengl scene for qml

--- openglwidgetqml.h
+++ openglwidgetqml.h
@@ -1,26 +1,157 @@
-#pragma once
+
+#ifndef OPENGLWIDGETQML
+#define OPENGLWIDGETQML
 
 #include <QQuickFramebufferObject>
 #include <QOpenGLFunctions>
+#include "ansyswrapper.h"
+
 
 class RenderOpenGL;
 
 class OpenGLWidgetQML : public QQuickFramebufferObject {
     Q_OBJECT
-    Q_PROPERTY(qreal t READ t WRITE setT NOTIFY tChanged)
 
     QML_ELEMENT
+protected:
+    static RenderOpenGL* m_render;
 
 public:
     explicit OpenGLWidgetQML(QQuickItem *parent = nullptr);
     Renderer *createRenderer() const override;
+    virtual ~OpenGLWidgetQML();
 
-    qreal t() const { return m_t; }
-    void setT(qreal t);
 
+    /**
+     * Set the voxel data.
+     * @param voxels 3D array of voxel data.
+     * @param numCubes Number of cubes per dimension.
+     */
+    void setVoxels(int32_t*** voxels, short int numCubes);
+
+    /**
+     * Retrieve the voxel data.
+     * @return Pointer to the 3D voxel array.
+     */
+    int32_t*** getVoxels();
+
+    QImage captureScreenshot();
+    QImage captureScreenshotWithWhiteBackground();
+    void captureScreenshotToClipboard();
+
+
+    std::vector<std::array<GLubyte, 4>> generateDistinctColors();
+
+    void setPlotWireFrame(bool status);
+    QVector<QColor> getColorMap(int numLevels);
+    void setComponent(int index);
+
+    int cubeSize = 1;
 signals:
-    void tChanged();
+protected:
+    struct Voxel;
+    void initializeGL();
+    void paintGL();
+    void drawAxis();
+    void initLights();
+    void resizeGL(int width, int height);
+    void paintSphere(float radius, int numStacks, int numSlices);
+    void drawCube(float cubeSize, GLenum type = GL_QUADS);
+    void drawCube(short cubeSize, Voxel vox, bool* neighbors, std::vector<std::array<GLubyte, 4>> &node_colors);
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
+    void resizeEvent(QResizeEvent *event);
+
+    void qNormalizeAngle(int &angle);
+
+    void calculateScene();
+    void initializeVBO();
+    void updateVBO();
+
+    std::vector<std::array<GLubyte, 4>> createColorMap(int numLevels);
+
+    std::array<GLubyte, 4> scalarToColor(float value, const std::vector<std::array<GLubyte, 4>>& colorMap);
+    ansysWrapper *wr;
+    int plotComponent;
+
+public slots:
+    // slots for xyz-rotation slider
+    void setXRotation(int angle);
+    void setYRotation(int angle);
+    void setZRotation(int angle);
+
+public:
+    void setFrontView();
+    void setBackView();
+    void setTopView();
+    void setBottomView();
+    void setLeftView();
+    void setRightView();
+    void setIsometricView();
+    void setDimetricView();
+
+    void setNumCubes(int numCubes);
+    void setNumColors(int numColors);
+    void setDistanceFactor(int factor);
+    void setDelayAnimation(int delayAnimation);
+    void setAnsysWrapper(ansysWrapper *wr);
+    void DelayFrameUpdate();
+
+    void setSceneParent(QQuickItem *parentItem);
+    void setParentWidget(QWidget *parent);
+signals:
+    // signaling rotation from mouse movement
+    void xRotationChanged(int angle);
+    void yRotationChanged(int angle);
+    void zRotationChanged(int angle);
+private:
+    QTimer* timer;
 
 private:
-    qreal m_t = 0.0;
+    GLuint vboIds[3];
+
+protected:
+    int xRot;
+    int yRot;
+    int zRot;
+    int delayAnimation;
+
+    float distance;
+    float zoomFactor = 1.0f;
+
+    QColor bgColor;
+
+    QPoint lastPos;
+    float distanceFactor = 0;
+
+    int32_t*** voxels;
+    short int numCubes;
+    int numColors;
+
+    std::vector<std::array<GLubyte, 4>> colors;
+    std::vector<float> directionFactors;
+
+    void updateVoxelColor(Voxel &v1);
+
+    bool isVBOupdateRequired = false;
+
+    struct Voxel
+    {
+        GLfloat x, y, z; // Coordinates
+        GLubyte r, g, b, a; // Color attributes
+        GLbyte nx, ny, nz; // Normal attributes
+    };
+    std::vector<Voxel> voxelScene;
+
+    bool plotWireFrame = false;
+
+
 };
+
+
+
+
+#endif
--- renderopengl.h
+++ renderopengl.h
@@ -3,14 +3,82 @@
 #include <QQuickFramebufferObject>
 #include <QOpenGLFunctions>
 #include <QOpenGLShaderProgram>
+#include "ansyswrapper.h"
 
-class RenderOpenGL : public QQuickFramebufferObject::Renderer, protected QOpenGLFunctions {
+/**
+ * @class RenderOpenGL
+ * A custom OpenGL widget for rendering 3D voxel scenes with interaction capabilities.
+ */
+
+class RenderOpenGL : public QQuickFramebufferObject::Renderer, protected QOpenGLFunctions, public QObject
+{
+
 public:
+
     RenderOpenGL();
+    virtual ~RenderOpenGL();
     void render() override;
     QSize size() const { return QSize(300, 300); }
 
+
 private:
-    QOpenGLShaderProgram m_program;
-    qreal m_t = 0.0;
+    /**
+     * OpenGL debug callback for logging messages.
+     */
+    static void debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                              GLsizei length, const GLchar* message, const void* userParam);
+
+public:
+
+    void setPlotWireFrame(bool status);
+    void setRotations(int xRot, int yRot, int zRot);
+    void setNumCubes(int numCubes);
+    void setDistZoomFactor(float distance, float zoomFactor);
+    void resizeGL(int width, int height);
+
+protected:
+    struct Voxel;
+    void initializeGL();
+    void paintGL();
+    void drawAxis();
+    void initLights();
+
+
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+
+    void initializeVBO();
+    void updateVBO();
+
+protected:
+
+    GLuint vboIds[3];
+
+    int xRot;
+    int yRot;
+    int zRot;
+
+    int numCubes;
+
+    float distance;
+    float zoomFactor = 1.0f;
+
+    QColor bgColor;
+
+
+    QMatrix4x4 m_projection;
+    float distanceFactor = 0;
+
+    bool isVBOupdateRequired = false;
+
+    struct Voxel
+    {
+        GLfloat x, y, z; // Coordinates
+        GLubyte r, g, b, a; // Color attributes
+        GLbyte nx, ny, nz; // Normal attributes
+    };
+    std::vector<Voxel> voxelScene;
+
+    bool plotWireFrame = false;
+
 };
